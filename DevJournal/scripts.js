73826e16caf5cdd90dc3e05a785a548d3174ea28
Augustus31 <supersumukh1@gmail.com>
2024-05-16 15:08:30 -0700

    Post functionality implemented

--- DevJournal/scripts.js
+++ DevJournal/scripts.js
@@ -1,19 +1,167 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const createButton = document.getElementById('create-post');
-    const main = document.querySelector('main');
+const main = document.querySelector('main');
+let temp = new Map();
+let idGen = 0;
 
-    // Function to create a new post
-    function createPost() {
-        const postDiv = document.createElement('div');
-        postDiv.className = 'post';
-        postDiv.innerHTML = `
-            <h2>New Post</h2>
-            <p>This is a newly created post.</p>
-            <span>Just now</span>
-        `;
-        main.appendChild(postDiv);
-    }
+const DEFAULT_POST_COLOR = "white";
+const DEFAULT_TEXTBOX_COLOR = "white";
+const SELECTED_POST_COLOR = "#e8e8e8";
+const SELECTED_TEXTBOX_COLOR = "#f5f5f5"
+
+/**
+ * Function to determine what to do on page load. Attaches event listener to the "create post" button, and restores user's posts from the SQL database.
+ */
+document.addEventListener('DOMContentLoaded', function() {    
+    const createButton = document.getElementById('create-post');
 
     // Event listener for the create post button
     createButton.addEventListener('click', createPost);
+
+    //TODO: Use SQL to restore old posts
+
 });
+
+/**
+ * Logic for clicking the right button of the two buttons on the bottom right of each post. 
+ * @param {*} event Event that triggered this function
+ */
+function rightButtonClicked(event){
+    let value = event.currentTarget.getAttribute("value");
+    let post =  event.currentTarget.parentNode.parentNode;
+    let header = post.querySelector('.header')
+    let content = post.querySelector('.content')
+
+    //Delete button pressed
+    if(value == 0){ 
+        post.remove();
+        
+        //TODO: update SQL with deletion
+    }
+    //Reject button pressed, while in edit mode (edit button pressed beforehand)
+    else if(value == 1){
+
+        //switches icons to reflect leaving edit mode
+        event.currentTarget.setAttribute('value', 0);
+        event.currentTarget.querySelector('img').setAttribute('src', "icons/delete.png");
+        event.currentTarget.parentNode.querySelector('.leftButton').setAttribute('value', 0);
+        event.currentTarget.parentNode.querySelector('.leftButton').querySelector('img').setAttribute('src', "icons/edit.png");
+
+        //post no longer editable
+        post.style.background = DEFAULT_POST_COLOR;
+        header.setAttribute('contenteditable', 'false');
+        content.setAttribute('contenteditable', 'false');
+
+        //post styling back to normal
+        header.style.background = DEFAULT_TEXTBOX_COLOR;
+        content.style.background = DEFAULT_TEXTBOX_COLOR;
+
+        //restores previous value of header and content, stored in temp
+        content.innerText = temp.get(event.currentTarget.parentNode.parentNode.id)[1]
+        header.innerText = temp.get(event.currentTarget.parentNode.parentNode.id)[0]
+
+        //garbage removal
+        temp.delete(event.currentTarget.parentNode.parentNode.id.toString());
+    }
+}
+
+/**
+ * Logic for clicking the left button of the two buttons on the bottom right of each post. 
+ * @param {*} event Event that triggered this function
+ */
+function leftButtonClicked(event){
+    let value = event.currentTarget.getAttribute("value");
+    let post =  event.currentTarget.parentNode.parentNode;
+    let header = post.querySelector('.header')
+    let content = post.querySelector('.content')
+
+    //Edit button pressed, enters edit mode
+    if(value == 0){
+
+        //Changes icons to relect switch to edit mode
+        event.currentTarget.setAttribute('value', 1);
+        event.currentTarget.querySelector('img').setAttribute('src', "icons/accept.png");
+        event.currentTarget.parentNode.querySelector('.rightButton').setAttribute('value', 1);
+        event.currentTarget.parentNode.querySelector('.rightButton').querySelector('img').setAttribute('src', "icons/reject.png");
+
+        //changes post styling and allows editing
+        post.style.background = SELECTED_POST_COLOR;
+        header.setAttribute('contenteditable', 'true');
+        content.setAttribute('contenteditable', 'true');
+        header.style.background = SELECTED_TEXTBOX_COLOR;
+        content.style.background = SELECTED_TEXTBOX_COLOR;
+
+        //adds original value of content and header to temp, so it can be accessed later if user does not save
+        temp.set(event.currentTarget.parentNode.parentNode.id.toString(), [header.innerText, content.innerText]);
+
+    }
+    //Accept button pressed while in edit mode
+    else if(value == 1){
+
+        //changes icons to reflect switch out of edit mode
+        event.currentTarget.setAttribute('value', 0);
+        event.currentTarget.querySelector('img').setAttribute('src', "icons/edit.png");
+        event.currentTarget.parentNode.querySelector('.rightButton').setAttribute('value', 0);
+        event.currentTarget.parentNode.querySelector('.rightButton').querySelector('img').setAttribute('src', "icons/delete.png");
+
+        //reverts post styling to normal and disables editing
+        post.style.background = DEFAULT_POST_COLOR;
+        header.setAttribute('contenteditable', 'false');
+        content.setAttribute('contenteditable', 'false');
+        header.style.background = DEFAULT_TEXTBOX_COLOR;
+        content.style.background = DEFAULT_TEXTBOX_COLOR;
+
+        //garbage cleaning
+        temp.delete(event.currentTarget.parentNode.parentNode.id.toString());
+
+        //TODO: save post to SQL
+
+    }
+}
+
+/**
+ * Creates a new post
+ */
+function createPost() {
+    const postDiv = document.createElement('div');
+    postDiv.className = 'post';
+    postDiv.id = idGen;
+    postDiv.innerHTML = `
+        <h2 class="header" contenteditable="false">New Post</h2>
+        <p class="content" contenteditable="false">Your text here</p>
+        <span class="time">Just now</span>
+        <div class="flex">
+            <button class="leftButton" value="0">
+                <img class="buttonIcon" src="icons/edit.png" alt="edit" border="0" />
+            </button>
+            <button class="rightButton" value="0">
+                <img class="buttonIcon" src="icons/delete.png" alt="edit" border="0" />
+            </button>
+        </div>
+    `;
+    main.appendChild(postDiv);
+
+    //Adds event handlers to post buttons
+    const leftbutton = postDiv.querySelector('.leftButton');
+    leftbutton.addEventListener('click', leftButtonClicked);
+    const rightbutton = postDiv.querySelector('.rightButton');
+    rightbutton.addEventListener('click', rightButtonClicked);
+
+    //Increments idGen to provide a unique ID to each new post. Refactors IDs if they go above 1 million.
+    if(idGen > 1000000){
+        posts = document.querySelectorAll('.post');
+        if(posts.length < idGen){
+            for(let i = 0; i < posts.length; i++){
+                temp.set(i.toString(), temp.get(posts[i].id))
+                posts[i].id = i;    
+            }
+            idGen = posts.length;
+
+            //TODO: update IDs in SQL
+        }
+        else{
+            idGen++;
+        }
+    }
+    else{
+        idGen++;
+    }
+}
