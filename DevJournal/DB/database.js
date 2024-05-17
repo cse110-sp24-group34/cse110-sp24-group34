c996239c6c50b1be18fb28f622fcc8afb2920977
MauricioESerrano <mauryeserrano@hotmail.com>
2024-05-17 15:59:56 -0700

    SqLite update

--- DevJournal/DB/database.js
+++ DevJournal/DB/database.js
@@ -14,72 +14,144 @@ let db = new sqlite3.Database('./test.db', sqlite3.OPEN_READWRITE, (err) => {
 
 //possible additions include: tags
 //create table BLANK
-sql = "CREATE TABLE IF NOT EXISTS entries(id INTEGER PRIMARY KEY, title, date, entry)"
+sql = "CREATE TABLE IF NOT EXISTS entries(id INTEGER PRIMARY KEY, title, date, entry, msid)"
 
 db.run(sql);
 
+
+
+
+
+
+
 //Insert data into table
-async function createPost(title, date, entry) {
+async function createPost(title, date, entry, msid) {
 
-    try {
+    let postId = await getPostId(title, date, entry, msid);
 
-        let postId = await getPostId(title, date, entry);
+    //if post already exists
+    if (postId != null) {
 
-        console.log(postId);
+        return console.error("duplicate");
 
-        //if post already exists
-        if (postId != null) {
-
-            return console.log("Post already exists");
-        }
-
-        sql = `INSERT INTO entries(title, date, entry) VALUES (?,?,?)`;
-        
-        db.run(sql, [title, date, entry], (err) => {
-            if (err) return console.error(err.message);
-        });
-
-    } catch (error) {
-
-        console.error('Error fetching post ID:', error);
     }
+
+    sql = `INSERT INTO entries(title, date, entry, msid) VALUES (?,?,?,?)`;
+    
+    db.run(sql, [title, date, entry, msid], (err) => {
+        if (err) return null;
+    });
+
+    return (await getPostId(title, date, entry, msid));
 }
 
+
+
+
+
+//get title,date,entry from id
+//param: id
+function getData(id) {
+
+    let sql = "SELECT title, date, entry, msid FROM entries WHERE id = ?" 
+
+    return new Promise((resolve,reject) => {
+
+        db.get(sql,[id], (err,row) => {
+            if (err) { 
+                console.error(err.message);
+                reject(err);
+                return;
+            }
+
+            resolve(row ? { title: row.title, date: row.date, entry: row.entry, msid: row.msid } : null);
+
+        });
+    });
+}
+
+
+
+
+//returns rows in array format
+//param: none
+function getRows() {
+
+    let sql = "SELECT * FROM entries";
+
+    return new Promise((resolve,reject) => {
+
+        let toReturn = [];
+
+        db.all(sql, [], (err, rows) => {
+            
+            if (err) { 
+            
+                console.error(err.message);
+                reject(err);
+                return;
+            }
+            
+            rows.forEach(row => toReturn.push(row));
+
+            return resolve(toReturn);
+        });
+
+    });
+}
+
+
+
+
 //delete data from table
+//param: id
 function deletePost(id) {
 
     sql = `DELETE FROM entries WHERE id = ?`;
 
     db.run(sql,[id],(err) => {
 
-        if (err) return console.error(err.message); }
+        if (err) return 1;
+        else return 0; }
     );
 }
 
+
+
+
 //update data in existing table
 //NOTE: need to add a way for other param to remain unchanged if only 1 or 2 of 3 param is affected
-async function updatePost(id, title, date, entry) {
+//OR 
+//save/pass in the parameters you want to keep here so it remains unchanged. 
+//maybe simply "updatePost(title = newTitle); is sufficent. need to test"
+async function updatePost(id, title, date, entry, msid) {
 
-    sql = "UPDATE entries SET title = ?, date = ?, entry = ? WHERE id = ?";
+    sql = "UPDATE entries SET title = ?, date = ?, entry = ?, msid = ? WHERE id = ?";
 
-    db.run(sql,[title,date,entry,id], (err) => {
+    db.run(sql,[title, date, entry, msid, id], (err) => {
         
-        if (err) return console.error(err.message);
-
+        if (err) return 1;
+        else return 0;
     });
 }
 
+
+
+
+
+
 //get post id based on title, date, entry
 //returns id of the post that corresponds to this entry
-//must be wrapped in try catch
-function getPostId(title, date, entry) {
+function getPostId(title, date, entry, msid) {
     
-    let sql = "SELECT id FROM entries WHERE title = ? AND date = ? AND entry = ?";
+    let sql = "SELECT id FROM entries WHERE title = ? AND date = ? AND entry = ? AND msid = ?";
 
     return new Promise((resolve, reject) => {
        
-        db.get(sql, [title, date, entry], (err, row) => {
+        db.get(sql, [title, date, entry, msid], (err, row) => {
+           
             if (err) {
+                
                 console.error(err.message);
                 reject(err);
                 return;
@@ -91,36 +163,24 @@ function getPostId(title, date, entry) {
     });
 }
 
-//get data, must be wrapped in try catch
-function getData(id) {
-    
-    let sql = "SELECT title, date, entry FROM entries WHERE id = ?" 
 
-    return new Promise((resolve,reject) => {
 
-        db.get(sql,[id], (err,row) => {
-            if (err) { 
-                console.error(err.message);
-                reject(err);
-                return;
-            }
 
-            resolve(row ? { title: row.title, date: row.date, entry: row.entry } : null);
 
-        });
-    });
-}
-
+//Developer usage ONLY
 
 //print out the table
 //param: none
 function databaseContent() {
+
     sql = `SELECT * FROM entries`;
 
     db.all(sql, [], (err, rows) => {
-       
+
         if (err) return console.error(err.message);
+
             rows.forEach((row) => {
+
             console.log(row);
         });
     });
@@ -128,11 +188,14 @@ function databaseContent() {
 
 
 //Drop table - deletes table, i.e. database that stores data. ideally dont uncomment and run it unless you want to quick reset
-// db.run("DROP TABLE entries");
+//db.run("DROP TABLE entries");
 
 // --------------------------------------------------
                 // Test Functions
 // --------------------------------------------------
 
-// createPost("Title3","date1","entry1");
-// tableContents();
+//createPost("Title1","date1","entry1");
+//createPost("Title2","date1","entry1");
+//createPost("Title3","date1","entry1");
+//databaseContent();
+
