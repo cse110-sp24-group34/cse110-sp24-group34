9710d1370a4b412598f99fd1d402b105d9c6b59f
Austin <acyeung@ucsd.edu>
2024-05-21 00:32:43 -0700

    Delete Test

--- __tests__/__tests__.js
+++ __tests__/__tests__.js
@@ -18,6 +18,42 @@ describe('Basic dev use', () => {
       expect(postCt + 1).toBe(postCtPost);
     });
 
+    it('Delete All Posts', async () => {
+      let postCt = await page.$$eval(".post", (posts) => {
+        return posts.length;
+      });
+      for (i=0; i < postCt; i++) {
+        let postDel = await page.$eval(".rightButton", (btnDel) => {
+          btnDel.click();
+          return btnDel.innerHTML;
+        });
+        console.log(`Delete ${i}`);
+      }
+      let postCtPost = await page.$$eval(".post", (posts) => {
+        return posts.length;
+      });
+      expect(postCtPost).toBe(0);
+    });
+
+    /*
+    it('Add title and body to post', async () => { //comebackto
+      const postList = await page.$$(".post");
+      let post = await postList[postList.length-1];
+      await post.$eval('.leftButton', (btn) => {
+        btn.click();
+      });
+      await post.focus("h2");
+      await page.keyboard.type('test')
+
+      post = await post.$eval("h2", (post) => {
+        return post.innerHTML;
+      });
+      console.log(post);
+
+      
+    });
+    */
+
     
 
 });
