ec8d9b0f0f23d726cd8f094e8354c4408449d0aa
Austin <acyeung@ucsd.edu>
2024-05-21 14:20:10 -0700

    Additional Tests

--- __tests__/__tests__.js
+++ __tests__/__tests__.js
@@ -35,25 +35,76 @@ describe('Basic dev use', () => {
       expect(postCtPost).toBe(0);
     });
 
-    /*
-    it('Add title and body to post', async () => { //comebackto
-      const postList = await page.$$(".post");
-      let post = await postList[postList.length-1];
+    it('Reload and ensure empty, add 1 and then ensure there', async () => {
+      await page.reload();
+
+      let postCt = await page.$$eval(".post", (posts) => {
+        return posts.length;
+      });
+      expect(postCt).toBe(0);
+
+      await page.$eval('#create-post', (btn) => {
+        btn.click();
+      });
+
+      let postCtPost = await page.$$eval(".post", (posts) => {
+        return posts.length;
+      });
+      expect(postCtPost).toBe(1);
+
+    });
+
+    it('Add title and body to post', async () => {
+      let post = await page.$(".post");
       await post.$eval('.leftButton', (btn) => {
         btn.click();
       });
-      await post.focus("h2");
-      await page.keyboard.type('test')
 
-      post = await post.$eval("h2", (post) => {
+      await page.$eval("h2", (title) => {
+        title.innerHTML = ' ';
+      });
+      await page.$eval("p", (title) => {
+        title.innerHTML = ' ';
+      });
+      await page.focus("h2");
+      await page.keyboard.type('Additional Text');
+      await page.focus("p");
+      await page.keyboard.type('Bottom Text');
+
+      await post.$eval('.leftButton', (btn) => {
+        btn.click();
+      });
+
+      let postTitle = await post.$eval("h2", (post) => {
         return post.innerHTML;
       });
-      console.log(post);
+      let postBody = await post.$eval("p", (post) => {
+        return post.innerHTML;
+      });
 
+      expect(postTitle).toBe("Additional Text");
+      expect(postBody).toBe("Bottom Text");
       
     });
-    */
 
-    
+
+    it('Attempt to edit Title and Text without clicking edit', async () => {
+      let post = await page.$(".post");
+      await page.focus("h2");
+      await page.keyboard.type('New and Improved Additional Text');
+      await page.focus("p");
+      await page.keyboard.type('Bottom Text 2.0');
+
+      let postTitle = await post.$eval("h2", (post) => {
+        return post.innerHTML;
+      });
+      let postBody = await post.$eval("p", (post) => {
+        return post.innerHTML;
+      });
+
+      expect(postTitle).toBe("Additional Text");
+      expect(postBody).toBe("Bottom Text");
+      
+    });
 
 });
